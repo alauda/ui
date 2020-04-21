c6533bd767967dc43eeaa89ddd10c01700e23119
Lihua Tang <lhtang@alauda.io>
2020-04-21 15:36:59 +0800

    feat(accordion): add lazy load to accordion item

--- src/accordion/accordion-item/accordion-item.component.ts
+++ src/accordion/accordion-item/accordion-item.component.ts
@@ -1,15 +1,19 @@
 import { CdkAccordionItem } from '@angular/cdk/accordion';
 import { UniqueSelectionDispatcher } from '@angular/cdk/collections';
 import {
+  AfterContentInit,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  ContentChild,
   Directive,
   EventEmitter,
   Input,
   Output,
+  TemplateRef,
   ViewEncapsulation,
 } from '@angular/core';
+import { filter, startWith, take } from 'rxjs/operators';
 
 import { AccordionComponent } from '../accordion.component';
 
@@ -17,6 +21,10 @@ import { AccordionComponent } from '../accordion.component';
   selector: '[auiAccordionItemHeader]',
 })
 export class AccordionItemHeaderDirective {}
+@Directive({
+  selector: '[auiAccordionContent]',
+})
+export class AccordionItemContentDirective {}
 @Component({
   selector: 'aui-accordion-item',
   templateUrl: 'accordion-item.component.html',
@@ -26,7 +34,8 @@ export class AccordionItemHeaderDirective {}
   preserveWhitespaces: false,
   viewProviders: [AccordionItemComponent],
 })
-export class AccordionItemComponent extends CdkAccordionItem {
+export class AccordionItemComponent extends CdkAccordionItem
+  implements AfterContentInit {
   @Input()
   hideToggle: false;
 
@@ -36,6 +45,14 @@ export class AccordionItemComponent extends CdkAccordionItem {
   @Output()
   expandedChange = new EventEmitter<boolean>();
 
+  @ContentChild(AccordionItemContentDirective, {
+    read: TemplateRef,
+    static: true,
+  })
+  _lazyContentTpl: TemplateRef<unknown>;
+
+  lazyContentTpl: TemplateRef<unknown>;
+
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
     accordion: AccordionComponent,
@@ -43,6 +60,21 @@ export class AccordionItemComponent extends CdkAccordionItem {
     uniqueSelectionDispatcher: UniqueSelectionDispatcher,
   ) {
     super(accordion, cdr, uniqueSelectionDispatcher);
+  }
+
+  ngAfterContentInit() {
+    if (this._lazyContentTpl) {
+      // Render the content as soon as the accordion becomes open.
+      this.opened
+        .pipe(
+          startWith(null!),
+          filter(() => this.expanded),
+          take(1),
+        )
+        .subscribe(() => {
+          this.lazyContentTpl = this._lazyContentTpl;
+        });
+    }
   }
 
   toggle() {
--- src/accordion/accordion.module.ts
+++ src/accordion/accordion.module.ts
@@ -7,6 +7,7 @@ import { IconModule } from '../icon/public-api';
 
 import {
   AccordionItemComponent,
+  AccordionItemContentDirective,
   AccordionItemHeaderDirective,
 } from './accordion-item/accordion-item.component';
 import { AccordionComponent } from './accordion.component';
@@ -16,12 +17,14 @@ import { AccordionComponent } from './accordion.component';
     AccordionComponent,
     AccordionItemComponent,
     AccordionItemHeaderDirective,
+    AccordionItemContentDirective,
   ],
   imports: [CommonModule, CdkAccordionModule, IconModule, ButtonModule],
   exports: [
     AccordionComponent,
     AccordionItemComponent,
     AccordionItemHeaderDirective,
+    AccordionItemContentDirective,
   ],
   providers: [],
 })
