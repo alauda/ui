4a7ccf1c427491d7b6b6453793d13ff8f525fdcf
JounQin <admin@1stg.me>
2020-09-25 16:05:28 +0800

    chore: remove unnecessary jest config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.html$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
-    },
-  },
   testMatch: ['<rootDir>/src/**/+(*.)+(spec|test).+(ts|js)?(x)'],
   coverageReporters: ['text', 'html'],
   coverageDirectory: '<rootDir>/coverage',
