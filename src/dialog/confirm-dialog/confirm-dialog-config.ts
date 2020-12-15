20b940584099031c8a5efc566095022a9894869a
liyouzhi <yzli@alauda.io>
2020-12-15 16:13:55 +0800

    feat: add confirm dialog config confirmButton, switch display confirm button

--- src/dialog/confirm-dialog/confirm-dialog-config.ts
+++ src/dialog/confirm-dialog/confirm-dialog-config.ts
@@ -3,6 +3,7 @@ import { ConfirmType } from '../dialog.types';
 export class ConfirmDialogConfig<T = unknown, R = unknown> {
   title: string;
   content?: string;
+  confirmButton? = true;
   cancelButton? = true;
   confirmType?: ConfirmType = ConfirmType.Primary;
   confirmText? = 'OK';
