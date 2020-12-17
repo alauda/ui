7d5c3f649344f1853b0c71abe4024c33301dac97
Lihua Tang <lhtang@alauda.io>
2020-12-17 14:22:42 +0800

    fix: aui option labelContext AIT-4714

--- src/select/base-select.ts
+++ src/select/base-select.ts
@@ -404,6 +404,13 @@ export abstract class BaseSelect<T>
       this.focusedOption.blur();
     }
     this.focusedOption = focusedOption;
+    // TODO: 避免 aui-option 中的 labelContext 的引用被修改，为什么？
+    // http://jira.alauda.cn/browse/AIT-4714
+    if (focusedOption?.labelContext) {
+      this.focusedOption.labelContext = {
+        ...(focusedOption.labelContext as Record<string, unknown>),
+      };
+    }
     if (this.focusedOption) {
       this.focusedOption.focus();
       this.scrollToOption(this.focusedOption);
