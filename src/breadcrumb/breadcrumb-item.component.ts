5306e4c7df0f9269b3fdf9bddf7e8b0a59b21679
Yongping <ypzhao@alauda.io>
2021-01-19 16:10:00 +0800

    feat: breadcrumb item context max width

--- src/breadcrumb/breadcrumb-item.component.ts
+++ src/breadcrumb/breadcrumb-item.component.ts
@@ -8,6 +8,7 @@ import {
 @Component({
   selector: 'aui-breadcrumb-item',
   templateUrl: './breadcrumb-item.component.html',
+  styleUrls: ['./breadcrumb-item.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   preserveWhitespaces: false,
