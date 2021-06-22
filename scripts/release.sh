33737584898348fd843d57ea95fe0e87e93b7a9b
JounQin <admin@1stg.me>
2021-06-22 11:28:55 +0000

    ci: publish beta is only available on non-forked PR

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,14 +3,18 @@
 PUBLISH_VERSION=$(node scripts/publish-version)
 NPM_TAG=$(node scripts/npm-tag)
 
+if [ "$NPM_TOKEN" = "" ]; then
+  echo "NPM_TOKEN is not available on PR from forked repository!"
+  echo "If you're a member of Alauda, just checkout a new branch instead."
+  exit 0
+fi
+
 if [ "$NPM_TAG" = "beta" ]; then
   SRC_PKG=package.json
   TMP=$(mktemp)
   jq ".version = \"$PUBLISH_VERSION\"" "$SRC_PKG" >"$TMP"
   mv -f "$TMP" "$SRC_PKG"
 else
-  git config --local user.email "action@github.com"
-  git config --local user.name "GitHub Action"
   yarn release --release-as "$PUBLISH_VERSION"
 fi
 
