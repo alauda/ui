34ff1d7ff9329326fb66a16f0ff78295e779453f
JounQin <admin@1stg.me>
2021-06-29 12:34:44 +0800

    ci: should target `latest` tag by default (#135)

--- scripts/npm-tag.js
+++ scripts/npm-tag.js
@@ -3,5 +3,5 @@ const version = process.env.PUBLISH_VERSION;
 if (version === 'beta' || /-beta([.-_]?\d*)$/.test(version)) {
   process.stdout.write('beta');
 } else {
-  process.stdout.write(version);
+  process.stdout.write('latest');
 }
