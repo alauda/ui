e956495d39d523ac960c8107b552b432b3f86562
JounQin <admin@1stg.me>
2021-06-29 14:53:48 +0800

    ci: integration with changesets (#137)

--- scripts/publish-branch.js
+++ scripts/publish-branch.js
@@ -4,8 +4,4 @@ const version = process.env.PUBLISH_VERSION;
 
 if (version.includes('-prod-')) {
   process.stdout.write(getProdVersion(version));
-} else if (version === 'beta' || /-beta([.-_]?\d*)$/.test(version)) {
-  process.stdout.write('beta');
-} else {
-  process.stdout.write('latest');
 }
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
 PUBLISH_VERSION=$(node scripts/publish-version)
+PUBLISH_BRANCH=$(node scripts/publish-branch)
 NPM_TAG=$(node scripts/npm-tag)
 
 if [ "$NPM_TOKEN" = "" ]; then
   echo "NPM_TOKEN is not available on PR from forked repository!"
   echo "If you're a member of Alauda, just checkout a new branch instead."
   exit 0
+else
+  npm set //registry.npmjs.org/:_authToken "$NPM_TOKEN"
 fi
 
-if [ "$NPM_TAG" = "beta" ]; then
+if [ "$NPM_TAG" = "latest" ]; then
+  echo "Publish latest tag via this script is not permitted anymore."
+  exit 1
+else
   SRC_PKG=package.json
   TMP=$(mktemp)
   jq ".version = \"$PUBLISH_VERSION\"" "$SRC_PKG" >"$TMP"
   mv -f "$TMP" "$SRC_PKG"
-else
-  yarn release --release-as "$PUBLISH_VERSION"
 fi
 
 yarn build
-npm set //registry.npmjs.org/:_authToken "$NPM_TOKEN"
+
+if [ "$PUBLISH_BRANCH" != "" ]; then
+  git push --follow-tags origin "$PUBLISH_BRANCH"
+fi
+
 npm publish release --tag "$NPM_TAG"
