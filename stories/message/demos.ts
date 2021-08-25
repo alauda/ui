9e1f52bc08bb0531df5ebf0524aa1813e57ae706
fengtianze <spacerover@outlook.com>
2021-08-25 16:14:24 +0800

    feat: new message

---
+++ stories/message/demos.ts
@@ -0,0 +1,33 @@
+import { MessageService } from '@alauda/ui';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+
+@Component({
+  template: `
+    <button
+      aui-button="primary"
+      (click)="message.info({ content: '这是一条消息提醒', duration: 5000 })"
+    >
+      消息提醒
+    </button>
+    <button
+      aui-button="success"
+      (click)="message.success('这是一个条成功消息')"
+    >
+      成功消息
+    </button>
+    <button aui-button="warning" (click)="message.warning('这是一条警告消息')">
+      警告消息
+    </button>
+    <button aui-button="danger" (click)="message.error('这是一条错误消息')">
+      错误消息
+    </button>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BasicDemoComponent implements OnDestroy {
+  constructor(public message: MessageService) {}
+
+  ngOnDestroy() {
+    this.message.removeAll();
+  }
+}
