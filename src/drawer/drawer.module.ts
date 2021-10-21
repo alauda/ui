f993eb00a696a57e16fab09a4636ac302bb0fb9f
Yongping <ypzhao@alauda.io>
2021-10-21 09:43:23 +0800

    feat: new drawer

---
+++ src/drawer/drawer.module.ts
@@ -0,0 +1,28 @@
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { IconModule } from '../icon/public-api';
+
+import { DrawerComponent } from './component/drawer.component';
+import { DrawerService } from './component/drawer.service';
+import {
+  DrawerContentDirective,
+  DrawerFooterDirective,
+  DrawerHeaderDirective,
+} from './component/helper-directives';
+
+const COMMON = [
+  DrawerComponent,
+  DrawerHeaderDirective,
+  DrawerContentDirective,
+  DrawerFooterDirective,
+];
+@NgModule({
+  imports: [CommonModule, IconModule, IconModule, OverlayModule],
+  declarations: COMMON,
+  exports: COMMON,
+  providers: [DrawerService],
+  entryComponents: [DrawerComponent],
+})
+export class DrawerModule {}
