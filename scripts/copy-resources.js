7100fcc0a75443c966c70e5648c8e6334aad2f9c
yinshuxun <sxyin@alauda.io>
2022-03-18 16:04:18 +0800

    chore: export preset scss

--- scripts/copy-resources.js
+++ scripts/copy-resources.js
@@ -12,6 +12,7 @@ gulp
     'src/theme/_base-var.scss',
     'src/theme/_pattern.scss',
     'src/theme/_var.scss',
+    'src/theme/_theme-preset.scss',
     'src/theme/_mixin.scss',
   ])
   .pipe(gulp.dest(dest));
