65235429ee83c2193b751a178b16c730f830c4fb
chenyang <yangchen@alauda.io>
2022-04-19 10:06:29 +0800

    feat: confirm dialog content support template

--- src/dialog/confirm-dialog/confirm-dialog-config.ts
+++ src/dialog/confirm-dialog/confirm-dialog-config.ts
@@ -1,8 +1,10 @@
+import { TemplateRef } from '@angular/core';
+
 import { BeforeAction, ConfirmType } from '../dialog.types';
 
 export class ConfirmDialogConfig<T = unknown, R = unknown> {
   title: string;
-  content?: string;
+  content?: string | TemplateRef<any>;
   cancelButton? = true;
   confirmType?: ConfirmType = ConfirmType.Primary;
   confirmText? = 'OK';
