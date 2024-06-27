cdbadede2ed2a8a031390f488d2352e835744651
yangxiaolang <yangxiaolang@users.noreply.github.com>
2024-06-27 07:11:07 +0000

    Deploy to GitHub pages

---
+++ sb-addons/storybook-9/manager-bundle.js
@@ -0,0 +1,3 @@
+try{
+var d=__STORYBOOKADDONS__,{addons:o,types:O,mockChannel:h}=__STORYBOOKADDONS__;var S=__STORYBOOKTHEMING__,{CacheProvider:f,ClassNames:u,Global:b,ThemeProvider:x,background:y,color:C,convert:N,create:a,createCache:R,createGlobal:k,createReset:B,css:D,darken:G,ensure:K,ignoreSsrWarning:P,isPropValid:Y,jsx:v,keyframes:A,lighten:I,styled:E,themes:H,typography:M,useTheme:j,withTheme:w}=__STORYBOOKTHEMING__;o.setConfig({panelPosition:"right",theme:a({base:"light",brandTitle:"Alauda UI"})});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
