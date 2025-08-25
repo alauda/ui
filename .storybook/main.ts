115354c7d0e9c2338494d2cba284c1a3d169a5cb
yangxiaolang <1810849666@qq.com>
2025-08-25 11:21:18 +0800

    chore: update ng and storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,21 +3,16 @@ import remarkGfm from 'remark-gfm';
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        mdxPluginOptions: {
-          mdxCompileOptions: {
-            remarkPlugins: [remarkGfm],
-          },
+  addons: ['@storybook/addon-links', {
+    name: '@storybook/addon-docs',
+    options: {
+      mdxPluginOptions: {
+        mdxCompileOptions: {
+          remarkPlugins: [remarkGfm],
         },
       },
     },
-  ],
+  }],
   framework: {
     name: '@storybook/angular',
     options: {},
