a570d99d25344e8db124d834970b7c44d8cd644e
fengtianze <fengtianze@icloud.com>
2026-01-12 14:48:36 +0800

    chore: update npm publish action

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,12 +4,10 @@ PUBLISH_VERSION=$(node scripts/publish-version)
 PUBLISH_BRANCH=$(node scripts/publish-branch)
 NPM_TAG=$(node scripts/npm-tag)
 
-if [ "$NPM_TOKEN" = "" ]; then
+if [ "$NODE_AUTH_TOKEN" = "" ] && [ "$NPM_TOKEN" = "" ]; then
   echo "NPM_TOKEN is not available on PR from forked repository!"
   echo "If you're a member of Alauda, just checkout a new branch instead."
   exit 0
-else
-  npm set //registry.npmjs.org/:_authToken "$NPM_TOKEN"
 fi
 
 if [ "$NPM_TAG" = "latest" ]; then
@@ -28,4 +26,4 @@ if [ "$PUBLISH_BRANCH" != "" ]; then
   git push --follow-tags origin "$PUBLISH_BRANCH"
 fi
 
-npm publish ./release --tag "$NPM_TAG"
+npm publish ./release --tag "$NPM_TAG" --provenance --access public
